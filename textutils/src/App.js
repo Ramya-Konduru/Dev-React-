9686c50d639bfb86ce68c40dc67203fa239abbe5
Ramya Konduru <ramyapatrudu26@gmail.com>
2022-04-28 23:42:25 +0530

    React components

---
+++ textutils/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar link="About Us" />
+    </>
+  );
+}
+
+export default App;
